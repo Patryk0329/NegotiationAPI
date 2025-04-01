f19db6acc2de055f340781b36d77d452c79f9cf6
Patryk Holubowicz <holubowiczpatryk@gmail.com>
2025-04-01 13:04:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.OpenApi;
+
+
+namespace NegotiationAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
